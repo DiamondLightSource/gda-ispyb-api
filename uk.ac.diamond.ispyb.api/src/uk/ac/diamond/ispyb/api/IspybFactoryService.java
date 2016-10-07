795bb5566e412e54f686fb58ebc04c0e03f75c8f
Peter Chang <peter.chang@diamond.ac.uk>
2016-10-07 10:36:20 +0100

    Add test that connects to real database

--- uk.ac.diamond.ispyb.api/src/uk/ac/diamond/ispyb/api/IspybFactoryService.java
+++ uk.ac.diamond.ispyb.api/src/uk/ac/diamond/ispyb/api/IspybFactoryService.java
@@ -2,7 +2,10 @@ package uk.ac.diamond.ispyb.api;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.Properties;
 
 public interface IspybFactoryService {
 	IspybApi build(String url, Optional<String> username, Optional<String> password) throws SQLException;
+
+	IspybApi build(String url, Properties properties) throws SQLException;
 }
