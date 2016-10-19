66be5dc611253f6c37cfa441b667ea9ab4a38a76
Paul Hathaway <paul.hathaway@diamond.ac.uk>
2016-10-19 16:18:30 +0100

    MXGDA-2882 Add Schema enum conversion from String
    Change-Id: I692663ecad10796a8c89afaa295372f66d6a07ab

--- uk.ac.diamond.ispyb.api/src/uk/ac/diamond/ispyb/api/Schema.java
+++ uk.ac.diamond.ispyb.api/src/uk/ac/diamond/ispyb/api/Schema.java
@@ -1,8 +1,19 @@
 package uk.ac.diamond.ispyb.api;
 
 public enum Schema {
-	ISPYBSTAGE, ISPYB;
-	
+	ISPYBSTAGE,
+	ISPYB,
+	INVALID;
+
+	public static Schema convert(String label) {
+		for (Schema s : Schema.values()) {
+			if (s.toString().equals(label)) {
+				return s;
+			}
+		}
+		return INVALID;
+	}
+
 	@Override
 	public String toString(){
 		return this.name().toLowerCase();
