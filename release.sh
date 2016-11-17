c255e63bb63f3200168ec8ac73f16ad3bb36c7fe
Kevin Savage <kevin.savage@diamond.ac.uk>
2016-11-17 10:53:10 +0000

    updating and testing release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="grep version pom.xml | head -1 | sed 's/\s<version>//g' | sed 's/<\/version>//g'"
+VERSION=$(grep version pom.xml | head -1 | sed 's/\s<version>//g' | sed 's/<\/version>//g')
 NEW_VERSION=$(echo $VERSION | perl -pe 's/^(\d+\.)(\d+\.)(\d+)$/$1.$2.($3+1)/e')
 
 echo "releasing version $VERSION" 
@@ -9,8 +9,12 @@ git push --tag
 
 echo "setting up next version $NEW_VERSION"
 
-find . -type f -name "*pom.xml" -exec sed -i 's/1.0.1/1.0.2/g' {} +
-find . -type f -name "*feature.xml" -exec sed -i 's/1.0.1/1.0.2/g' {} +
-find . -type f -name "*MANIFEST.MF" -exec sed -i 's/1.0.1/1.0.2/g' {} +
+REPLACE_PATTERN=$(echo s/$VERSION/$NEW_VERSION/g | sed 's/\./\\\./g')
 
+find . -type f -name "*pom.xml" -exec sed -i "$REPLACE_PATTERN" {} +
+find . -type f -name "*feature.xml" -exec sed -i "$REPLACE_PATTERN" {} +
+find . -type f -name "*MANIFEST.MF" -exec sed -i "$REPLACE_PATTERN" {} +
 
+git add .
+git commit -m "updating version to $NEW_VERSION"
+git push origin master
