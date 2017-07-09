3bc39dc07cc4e88009a80244410173bb80eac738
Karl Levik <karl.levik@diamond.ac.uk>
2017-07-09 12:10:46 +0100

    Missing file from last commit

---
+++ uk.ac.diamond.ispyb.api/src/uk/ac/diamond/ispyb/api/Position.java
@@ -0,0 +1,81 @@
+/*-
+ *******************************************************************************
+ * Copyright (c) 2011, 2016 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    See git history
+ *******************************************************************************/
+package uk.ac.diamond.ispyb.api;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+public class Position {
+    
+	private Long dcId;
+	private Double posX;
+	private Double posY;
+	private Double posZ;
+	private Double scale;
+	
+    public Long getDCId() {
+        return dcId;
+    }
+
+    public void setDCId(Long dcId) {
+        this.dcId = dcId;
+    }
+
+	public Double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(Double posX) {
+		this.posX = posX;
+	}
+
+	public Double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(Double posY) {
+		this.posY = posY;
+	}
+
+	public Double getPosZ() {
+		return posZ;
+	}
+
+	public void setPosZ(Double posZ) {
+		this.posZ = posZ;
+	}
+
+	public Double getScale() {
+		return scale;
+	}
+
+	public void setScale(Double scale) {
+		this.scale = scale;
+	}
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that);
+    };
+
+}
