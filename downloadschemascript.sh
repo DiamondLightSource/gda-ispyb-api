3ebcd9c7e64420519ae69d85661f4ada1f49bf34
Karl Levik <KarlLevik@users.noreply.github.com>
2019-08-30 13:10:59 +0100

    Bug fix and use ispyb-database version 1.6.2

--- downloadschemascript.sh
+++ downloadschemascript.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-version=1.6.0
+version=1.6.2
 fname=ispyb-database-${version}.tar.gz
 
 cd target/test-classes
 
-if [ ! -d "schema" ]; then
-  if [ ! -f ${fname} ]; then
+if [ \( ! -d "schema" \) -o \( ! -f ${fname} \)  ]; then
     wget https://github.com/DiamondLightSource/ispyb-database/releases/download/v${version}/${fname}
     tar xfz ${fname}
-  fi
 fi
