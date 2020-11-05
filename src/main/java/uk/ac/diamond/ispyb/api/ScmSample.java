5e13163a8ef3453759ad0dcad1046ddacde2eb22
Karl Levik <KarlLevik@users.noreply.github.com>
2020-11-04 18:08:43 -0600

    Add more attributes for ScmSample

--- src/main/java/uk/ac/diamond/ispyb/api/ScmSample.java
+++ src/main/java/uk/ac/diamond/ispyb/api/ScmSample.java
@@ -11,25 +11,49 @@
  *******************************************************************************/
 package uk.ac.diamond.ispyb.api;
 
+import java.util.List;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 public class ScmSample {
     private Long sampleId;
+    private Long materialId;
     private Long containerId;
-    private Long dataCollectionPlanId;
+    private Long planId;
     private Long sessionId;
     private Long proposalId;
     private String sampleName;
     private String sampleCode;
+    private Float sampleVolume;
+    private Double thickness;
+    private String samplePurificationColumn;
     private String sampleComments;
     private String sampleLocation;
     private String sampleSubLocation;
     private String sampleStatus;
+    private String materialName;
+    private String materialAcronym;
+    private String materialFormula;
+    private Float materialDensity;
+    private String materialSafetyLevel; // enum(GREEN, YELLOW, RED)
+    private String materialChemicalDescription;
+    private Double materialMolecularMass;
+    private String materialConcentrationType;
+    private String materialType; // protein, DNA, RNA, small molecule, ...
+    private String materialIsotropy; // enum('isotropic','anisotropic')
+    private List<String> materialSubTypes; // will be hard to populate ...
+    private String planExperimentType;
+    private String planPurificationColumn;
+    private Float planRobotPlateTemperature;
+    private Float planExposureTemperature;
+    private Float planTransmission;
     private String proposalCode;
     private String proposalNumber;
     private Long sessionNumber;
+
+    // Getters and setters
 
     public Long getSampleId() {
         return sampleId;
@@ -37,6 +61,14 @@ public class ScmSample {
 
     public void setSampleId(Long sampleId) {
         this.sampleId = sampleId;
+    }
+
+    public Long getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(Long materialId) {
+        this.materialId = materialId;
     }
 
     public Long getContainerId() {
@@ -47,12 +79,12 @@ public class ScmSample {
         this.containerId = containerId;
     }
 
-    public Long getDataCollectionPlanId() {
-        return dataCollectionPlanId;
+    public Long getPlanId() {
+        return planId;
     }
 
-    public void setDataCollectionPlanId(Long dataCollectionPlanId) {
-        this.dataCollectionPlanId = dataCollectionPlanId;
+    public void setPlanId(Long planId) {
+        this.planId = planId;
     }
 
     public Long getProposalId() {
@@ -143,6 +175,158 @@ public class ScmSample {
         this.sessionNumber = sessionNumber;
     }
 
+    public Float getSampleVolume() {
+        return sampleVolume;
+    }
+
+    public void setSampleVolume(Float sampleVolume) {
+        this.sampleVolume = sampleVolume;
+    }
+
+    public Double getThickness() {
+        return thickness;
+    }
+
+    public void setThickness(Double thickness) {
+        this.thickness = thickness;
+    }
+
+    public String getSamplePurificationColumn() {
+        return samplePurificationColumn;
+    }
+
+    public void setSamplePurificationColumn(String samplePurificationColumn) {
+        this.samplePurificationColumn = samplePurificationColumn;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public String getMaterialAcronym() {
+        return materialAcronym;
+    }
+
+    public void setMaterialAcronym(String materialAcronym) {
+        this.materialAcronym = materialAcronym;
+    }
+
+    public String getMaterialFormula() {
+        return materialFormula;
+    }
+
+    public void setMaterialFormula(String materialFormula) {
+        this.materialFormula = materialFormula;
+    }
+
+    public Float getMaterialDensity() {
+        return materialDensity;
+    }
+
+    public void setMaterialDensity(Float materialDensity) {
+        this.materialDensity = materialDensity;
+    }
+
+    public String getMaterialSafetyLevel() {
+        return materialSafetyLevel;
+    }
+
+    public void setMaterialSafetyLevel(String materialSafetyLevel) {
+        this.materialSafetyLevel = materialSafetyLevel;
+    }
+
+    public String getMaterialChemicalDescription() {
+        return materialChemicalDescription;
+    }
+
+    public void setMaterialChemicalDescription(String materialChemicalDescription) {
+        this.materialChemicalDescription = materialChemicalDescription;
+    }
+
+    public Double getMaterialMolecularMass() {
+        return materialMolecularMass;
+    }
+
+    public void setMaterialMolecularMass(Double materialMolecularMass) {
+        this.materialMolecularMass = materialMolecularMass;
+    }
+
+    public String getMaterialConcentrationType() {
+        return materialConcentrationType;
+    }
+
+    public void setMaterialConcentrationType(String materialConcentrationType) {
+        this.materialConcentrationType = materialConcentrationType;
+    }
+
+    public String getMaterialType() {
+        return materialType;
+    }
+
+    public void setMaterialType(String materialType) {
+        this.materialType = materialType;
+    }
+
+    public String getMaterialIsotropy() {
+        return materialIsotropy;
+    }
+
+    public void setMaterialIsotropy(String materialIsotropy) {
+        this.materialIsotropy = materialIsotropy;
+    }
+
+    public List<String> getMaterialSubType() {
+        return materialSubTypes;
+    }
+
+    public void setMaterialSubType(List<String> materialSubType) {
+        this.materialSubTypes = materialSubType;
+    }
+
+    public String getPlanExperimentType() {
+        return planExperimentType;
+    }
+
+    public void setPlanExperimentType(String planExperimentType) {
+        this.planExperimentType = planExperimentType;
+    }
+
+    public String getPlanPurificationColumn() {
+        return planPurificationColumn;
+    }
+
+    public void setPlanPurificationColumn(String planPurificationColumn) {
+        this.planPurificationColumn = planPurificationColumn;
+    }
+
+    public Float getPlanRobotPlateTemperature() {
+        return planRobotPlateTemperature;
+    }
+
+    public void setPlanRobotPlateTemperature(Float planRobotPlateTemperature) {
+        this.planRobotPlateTemperature = planRobotPlateTemperature;
+    }
+
+    public Float getPlanExposureTemperature() {
+        return planExposureTemperature;
+    }
+
+    public void setPlanExposureTemperature(Float planExposureTemperature) {
+        this.planExposureTemperature = planExposureTemperature;
+    }
+
+    public Float getPlanTransmission() {
+        return planTransmission;
+    }
+
+    public void setPlanTransmission(Float planTransmission) {
+        this.planTransmission = planTransmission;
+    };
+
     @Override
     public String toString() {
         return ReflectionToStringBuilder.toString(this);
@@ -156,6 +340,6 @@ public class ScmSample {
     @Override
     public boolean equals(Object that) {
         return EqualsBuilder.reflectionEquals(this, that);
-    };
+    }
 
 }
